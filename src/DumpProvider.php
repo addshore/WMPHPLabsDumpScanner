d42b53b7f2e7b94fbf2b4978b2fb5c00649566ea
addshore <addshorewiki@gmail.com>
2014-02-21 16:32:16 +0100

    Make and use globRecursive method

--- src/DumpProvider.php
+++ src/DumpProvider.php
@@ -8,7 +8,17 @@ class DumpProvider {
 	 * @return array
 	 */
 	public function get() {
-		return glob( DUMPSCAN_DUMPS . DIRECTORY_SEPARATOR . '*.xml');
+		return $this->rglob( DUMPSCAN_DUMPS . DIRECTORY_SEPARATOR . '*.xml');
+	}
+
+	private function rglob( $pattern, $flags = 0 )
+	{
+		$files = glob( $pattern, $flags );
+		foreach ( glob( dirname( $pattern ) . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR|GLOB_NOSORT ) as $dir )
+		{
+			$files = array_merge( $files, $this->rglob( $dir . DIRECTORY_SEPARATOR . basename( $pattern ), $flags ) );
+		}
+		return $files;
 	}
 
 } 
