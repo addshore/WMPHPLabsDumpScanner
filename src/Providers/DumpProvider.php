5b66ab19544ff7c473d80b80dad9c3429bfb0a10
addshore <addshorewiki@gmail.com>
2014-02-22 13:48:50 +0100

    Add really basic caching to DumpProvider

--- src/Providers/DumpProvider.php
+++ src/Providers/DumpProvider.php
@@ -5,10 +5,18 @@ namespace DumpScan\Providers;
 class DumpProvider {
 
 	/**
+	 * Get list of dumps with REALLY basic caching
 	 * @return array
 	 */
 	public function get() {
-		return glob( DUMPSCAN_DUMPS );
+		$tmp = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'LabsDumpScanner.DumpProvider.' . date("dmYH");
+		if( file_exists( $tmp ) ) {
+			return explode( "\n", file_get_contents( $tmp ) );
+		} else {
+			$dumps = glob( DUMPSCAN_DUMPS );
+			file_put_contents( $tmp, implode( "\n", $dumps ) );
+			return $dumps;
+		}
 	}
 
 } 
